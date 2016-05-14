004ef0ea54f9b0f8079ac7ba15a81515acf816cf
BFAlex <838318741@qq.com>
2016-05-14 14:18:35 +0800

    Initial Commit

---
+++ BFCustomPageViewCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BFCustomPageViewCode
+//
+//  Created by Readboy_BFAlex on 16/5/14.
+//  Copyright © 2016年 BF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BFCustomPageViewCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BFCustomPageViewCode
+//
+//  Created by Readboy_BFAlex on 16/5/14.
+//  Copyright © 2016年 BF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
